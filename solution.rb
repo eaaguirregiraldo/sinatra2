4717b69262dd2c9201ef8e6e2baf71d1b42e3324
eaaguirregiraldo <eaaguirregiraldo@gmail.com>
2016-02-02 17:06:16 -0500

    Version inicial

---
+++ solution.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+get '/maker/:nombre' do
+ unless params[:nombre]
+  <<-HTML
+    <h1>Hola desconocido!</h1>
+  HTML
+ else
+   @greeting_name = params[:nombre]
+
+   <<-HTML
+     <h1>Hola #{@greeting_name.capitalize}!</h1>
+   HTML
+   
+ end
+end
